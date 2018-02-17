1001b6de83201e818ccf765341aff2198a8a2e6e
mengyuhan <simonhmy@gmail.com>
2018-02-17 13:54:59 -0800

    create miner key file

--- artappTest/art-app233.go
+++ artappTest/art-app233.go
@@ -40,37 +40,36 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Print(canvas, "ignore")
+	//fmt.Print(canvas, "ignore")
 	validateNum := uint8(2)
 
 	// Add a square.
-	shapeHash, blockHash, ink, err := canvas.AddShape(validateNum, blockartlib.PATH, "M 600 600 l 39 0 v 39 h -39 z", "filled", "red")
+	_, _, ink, err := canvas.AddShape(validateNum, blockartlib.PATH, "M 600 600 l 39 0 v 39 h -39 z", "filled", "red")
 	if checkError(err) != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Print(shapeHash, blockHash, ink)
+	fmt.Printf("after add a square, ink remaining is %d\n", ink)
 
 	// // Add 凸
-	shapeHash2, blockHash2, ink2, err := canvas.AddShape(validateNum, blockartlib.PATH, "M 800 800 l 50 0 l 0 50 h 50 v 50  h -150 v -50 h 50 z", "transparent", "blue")
+	_, _, ink2, err := canvas.AddShape(validateNum, blockartlib.PATH, "M 800 800 l 50 0 l 0 50 h 50 v 50  h -150 v -50 h 50 z", "transparent", "blue")
 	if checkError(err) != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Print(shapeHash2, blockHash2, ink2)
+	fmt.Printf("after add 凸, ink remaining is %d\n", ink2)
 	// // Add 凹
-	shapeHash3, blockHash3, ink3, err := canvas.AddShape(validateNum, blockartlib.PATH, "M 500 500 l 30 0 l 0 30 h 30 v -30  h 30 v 60 h -90 z", "transparent", "green")
+	_, _, ink3, err := canvas.AddShape(validateNum, blockartlib.PATH, "M 500 500 l 30 0 l 0 30 h 30 v -30  h 30 v 60 h -90 z", "transparent", "green")
 	if checkError(err) != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Print(shapeHash3, blockHash3, ink3)
+	fmt.Printf("after add 凹, ink remaining is %d\n", ink3)
 
-	ink4, err := canvas.CloseCanvas()
+	_, err := canvas.CloseCanvas()
 	if checkError(err) != nil {
 		return
 	}
-	fmt.Println(ink4)
 }
 
 // If error is non-nil, print it out and return it.
